c80fcf206a836142f86e81aa97150df01d8d57e8
Lara <75126217+little-evil-genius@users.noreply.github.com>
2024-04-22 10:59:53 +0200

    Update teamheader.lang.php

--- inc/languages/deutsch_du/teamheader.lang.php
+++ inc/languages/deutsch_du/teamheader.lang.php
@@ -1,5 +1,5 @@
 <?php
 $l['teamheader_headline'] = "Unsere Teammitglieder";
 $l['teamheader_lastvisit_away'] = "ist aktuell abwesend!";
-$l['teamheader_lastvisit_returndate'] = "Bis zum {1} abwesend!";
+$l['teamheader_lastvisit_returndate'] = "bis zum {1} abwesend!";
 $l['teamheader_lastvisit_offline'] = "ist aktuell offline!";
